f8561211fc17a0d51242faa9cb9f3e0688066228
Daniel Merkl <dan.merkl.90@gmail.com>
2019-12-30 10:44:23 -0600

    adding some jquery

---
+++ assets/js/todos.js
@@ -0,0 +1,18 @@
+// Check off specific Todos By Clicking
+$("li").click(function () {
+    // if li is gray
+    if($(this).css("color") === "rgb(128, 128, 128)"){
+        $(this).css({
+            color: "black",
+            textDecoration: "none"
+        
+        });
+    }
+else { 
+    $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    
+    });
+}
+});
