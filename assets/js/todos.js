00c3be1802ef20b6a543a546a15c46b58a691fbf
Daniel Merkl <dan.merkl.90@gmail.com>
2019-12-30 10:56:51 -0600

    adding functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,12 @@
 // Check off specific Todos By Clicking
-$("li").click(function () {
-    // if li is gray
-    if($(this).css("color") === "rgb(128, 128, 128)"){
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        
-        });
-    }
-else { 
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-    
-    });
-}
+$("li").click(function() {
+$(this).toggleClass("completed")
 });
+
+//click on x to delete todo
+$("span").click(function(event) {
+$(this).parent().fadeOut(500,function(){
+    $(this).remove();
+})
+event.stopPropagation();
+})
