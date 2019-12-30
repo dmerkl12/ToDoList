7ba4e1caadb98eaa2a559c1d096c3f6cbb29ad90
Daniel Merkl <dan.merkl.90@gmail.com>
2019-12-30 13:03:42 -0600

    few more things

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,10 @@
 // Check off specific Todos By Clicking
-$("ul").on("click", "li", function () {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed")
 });
 
 //click on x to delete todo
-$("ul").on("click", "span", function (event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     })
@@ -12,11 +12,15 @@ $("ul").on("click", "span", function (event) {
 })
 
 //
-$("input[type='text']").keypress(function (event) {
+$("input[type='text']").keypress(function(event) {
     if (event.which === 13) {
         let todoText = $(this).val();
         $(this).val("")
         //create new li and add to ul
-        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
 })
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
